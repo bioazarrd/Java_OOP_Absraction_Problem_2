a10b4c0b1f4475f9d4cfd1edc52018cb46e8a154
bioazarrd <74237466+bioazarrd@users.noreply.github.com>
2020-11-11 10:26:13 +0200

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,19 @@
+package D_JavaAdvancedOOP.Lecture1_Abstraction.ExProblem7;
+public class Point {
+
+    private int coordinateX;
+    private int coordinateY;
+
+    public Point(int coordinateX, int coordinateY) {
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+    public int getCoordinateX() {
+        return coordinateX;
+    }
+
+    public int getCoordinateY() {
+        return coordinateY;
+    }
+}
